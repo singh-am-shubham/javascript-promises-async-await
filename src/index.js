117a240539156f445b0243bba5235dfcd589bd5a
shubham <shubham22293@gmail.com>
2020-06-10 21:20:17 +0530

    all and race promise

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import { fetchWithTimeout } from "./services";
+import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
+
 const movies = require("./data/movies.json");
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+
+function getBooksAndMovies() {
+  return Promise.all([fetchMovies(), fetchBooks()])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch((error) => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
+function getBooksOrMovies() {
+    return Promise.race([fetchMovies(), fetchBooks()])
+    .then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+  }
 
-moviePromise.then((results) => {
-  console.log(results);
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then((results) => {
+  console.log("getBooksAndMoviesPromise", results);
 });
 
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then((results) => {
+  console.log("getBooksOrMoviesPromise", results);
+});
+
+// export function fetchMovies() {
+//     const resolveFunction = () => movies;
+//     return fetchWithTimeout(1000).then(resolveFunction);
+//   }
+//its async await form
 // export async function fetchMovies() {
 //     const resolveFunction = () => movies;
 //     const result = await fetchWithTimeout(1000);
