03b19f2385f240a5f2acda6add51190e51ceee92
shubham <shubham22293@gmail.com>
2020-06-10 20:59:20 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import {fetchWithTimeout} from './services';
-const movies = require("./data/movies.json")
+import { fetchWithTimeout } from "./services";
+const movies = require("./data/movies.json");
 export function fetchMovies() {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000)
-    .then(resolveFunction);
+  const resolveFunction = () => movies;
+  return fetchWithTimeout(1000).then(resolveFunction);
 }
 
 const moviePromise = fetchMovies();
 
-moviePromise.then(results => console.log(results));
+moviePromise.then((results) => {
+  console.log(results);
+});
 
 // export async function fetchMovies() {
 //     const resolveFunction = () => movies;
